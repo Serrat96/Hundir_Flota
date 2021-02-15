bacfa659b035a6cf11103749c6ff1a7b18497aa2
Serrat96 <serrat_torres@hotmail.com>
2021-02-15 17:32:03 +0100

    Commit 17:30 para irnos a casa

--- Main.py
+++ Main.py
@@ -1,11 +1,24 @@
 
 import Tablero
-import Barco
+from Barco import *
+import numpy as np
+#barco = Barco((1,'F'),1)
+array_barcos_1_pos = np.array([Barco((1,'C'), 1), Barco((3,'H'), 1), Barco((6,'I'), 1), Barco((8,'C'), 1)])
 
-array_barcos_1_pos = [Barco((1,'C'), 1), Barco((3,'H'), 1), Barco((6,'I'), 1), Barco((8,'C'), 1)]
+array_barcos_2_pos = np.array([Barco((1,'F'), 2, 0), Barco((1,'I'), 2, 0), Barco((10,'G'), 2, 1)])
 
-array_barcos_2_pos = [Barco((1,'F'), 2, 0), Barco((1,'I'), 2, 0), Barco((10,'G'), 2, 1)]
+array_barcos_3_pos = np.array([Barco((3,'A'), 3, 0), Barco((5,'E'), 3, 1)])
 
-array_barcos_3_pos = [Barco((3,'A'), 3, 0), Barco((5,'E'), 3, 1)]
+array_barcos_4_pos = np.array([Barco((8,'F'), 4, 0)])
 
-array_barcos_4_pos = [Barco((8,'F'), 4, 0)]
+array_barcos_total = np.concatenate((array_barcos_1_pos, array_barcos_2_pos, array_barcos_3_pos, array_barcos_4_pos))
+
+"""tablero_barco_j1 = Tablero(10, )
+
+tablero_disparos_j1 =
+
+tablero_barco_j2 =
+
+tablero_disparos_j2 ="""
+
+print(array_barcos_total)
--- Tablero.py
+++ Tablero.py
@@ -1,7 +1,8 @@
 import numpy as np
+
 class Tablero:
 
-    def __init__(self, dimension, barcos):
+    def __init__(self, dimension,  barcos):
 
-        self.matriz = np.zeros(100).reshape(10, 10)
+        self.matriz = np.zeros(dimension**2).reshape(dimension, dimension)
         self.barcos = barcos
